77547d92d1494de3a16d720cbc293377dfae7637
PlanetSat0rn <109532054+PlanetSat0rn@users.noreply.github.com>
2022-08-05 13:53:05 -0400

    Create LogicPlus.java

---
+++ src/planetsat0rn/LogicPlus.java
@@ -0,0 +1,14 @@
+package planetsat0rn.logicplus;
+
+import mindustry.mod.Mod;
+
+public class LogicPlus extends Mod{
+
+    @Override
+    public void init(){
+    }
+
+    @Override
+    public void loadContent(){
+    }
+}
